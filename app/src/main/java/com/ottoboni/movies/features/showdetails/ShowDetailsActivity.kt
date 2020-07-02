52c3cfb7efe9c7640450de609570b68c9d3311d3
Carlos Ottoboni <c.ottobonijr@gmail.com>
2020-07-02 16:12:13 -0300

    Add Show Details layout

---
+++ app/src/main/java/com/ottoboni/movies/features/showdetails/ShowDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.ottoboni.movies.features.showdetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ottoboni.movies.R
+
+class ShowDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_details)
+    }
+}
